9b97d6d8301301f0674ad87cb3b866bf0c6230fa
Kirisanth Subramaniam <kirisanthsubramaniam@Kirisanths-MacBook-Pro.local>
2013-11-23 17:46:37 -0500

    Initial Commit

---
+++ Assignment-3/Assignment-3/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Assignment-3
+//
+//  Created by Kirisanth Subramaniam on 2013-11-23.
+//  Copyright (c) 2013 Kirisanth Subramaniam. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
