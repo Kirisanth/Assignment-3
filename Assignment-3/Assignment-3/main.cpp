e47af035c47d4461ed6ac7f4f7205ac73776940a
Meraj Patel <meraj0@Merajs-MacBook-Pro-2.local>
2013-11-23 18:19:20 -0500

    basics

--- Assignment-3/Assignment-3/main.cpp
+++ Assignment-3/Assignment-3/main.cpp
@@ -6,14 +6,171 @@
 //  Copyright (c) 2013 Kirisanth Subramaniam. All rights reserved.
 //
 
+#include <stdlib.h>
+#include <GLUT/glut.h>
 #include <iostream>
+int ang = 0;	//angle for rotating cube
+int cnt = 0;
 
-int main(int argc, const char * argv[])
+/* drawCube() -- draws a cube with different coloured sides using QUAD primitives */
+void drawCube()
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    std::cout << "test!\n";
-    return 0;
+	glBegin(GL_QUADS);
+	
+    //front
+    glColor3f(1, 0, 0);
+    glVertex3f(-1, -1, 1);
+    glVertex3f(1, -1, 1);
+    glVertex3f(1, 1, 1);
+    glVertex3f(-1, 1, 1);
+    
+    //top
+    glColor3f(1, 1, 0);
+    glVertex3f(-1,1,1);
+    glVertex3f(1,1,1);
+    glVertex3f(1,1,-1);
+    glVertex3f(-1,1,-1);
+    
+    //bottom
+    glColor3f(0, 1, 0);
+    glVertex3f(-1,-1,1);
+    glVertex3f(1,-1,1);
+    glVertex3f(1,-1,-1);
+    glVertex3f(-1,-1,-1);
+    
+    //left side
+    glColor3f(0, 0, 1);
+    glVertex3f(-1,1,1);
+    glVertex3f(-1,-1,1);
+    glVertex3f(-1,-1,-1);
+    glVertex3f(-1,1,-1);
+    
+    //right side
+    glColor3f(1,0,1);
+    glVertex3f(1,1,1);
+    glVertex3f(1,-1,1);
+    glVertex3f(1,-1,-1);
+    glVertex3f(1,1,-1);
+    
+    //back side
+    glColor3f(0,1,0);
+    glVertex3f(-1,1,-1);
+    glVertex3f(-1,-1,-1);
+    glVertex3f(1,-1,-1);
+    glVertex3f(1,1,-1);
+    
+	glEnd();
 }
 
+/* drawAxis() -- draws an axis at the origin of the coordinate system
+ *   red = +X axis, green = +Y axis, blue = +Z axis
+ */
+void drawAxis()
+{
+	glBegin(GL_LINES);
+    glColor3f(1, 0, 0);
+    glVertex3f(0,0,0);
+    glVertex3f(50,0,0);
+    
+    glColor3f(0,1,0);
+    glVertex3f(0,0,0);
+    glVertex3f(0,50,0);
+    
+    glColor3f(0,0,1);
+    glVertex3f(0,0,0);
+    glVertex3f(0,0,50);
+	glEnd();
+}
+
+/* display() - the OpenGL display function, this draws the screen
+ *  it displays a spinning cube
+ */
+void display()
+{
+	//clear the screen
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+	cnt++;
+	//increment the rotation angle every 25th display cycle
+	if(cnt % 100 == 0)
+	{
+		ang++;
+		cnt = 0;
+	}
+    
+	//optionally draw the axis
+	drawAxis();
+    
+    
+	//push the current modelview matrix onto the matrix stack
+	//  this allows us to rotate, then pop the stack so as to only
+	//  rotate our cube, and only by the specified amount
+	glPushMatrix();
+    
+	//do the rotation - rotate about the Y axis by angle ang
+	
+	glRotatef(ang, 0, 1, 0);
+	
+    //	glTranslatef(1, 0, 0);
+	//draw the cube
+	drawCube();
+    
+	//pop the matrix back to what it was prior to the rotation
+	glPopMatrix();
+	
+	//swap buffers - rendering is done to the back buffer, bring it forward to display
+	glutSwapBuffers();
+    
+	//force a redisplay, to keep the animation running
+	glutPostRedisplay();
+}
+
+/* kbd -- the GLUT keyboard function
+ *  key -- the key pressed
+ *  x and y - mouse x and y coordinates at the time the function is called
+ */
+void kbd(unsigned char key, int x, int y)
+{
+	//if the "q" key is pressed, quit the program
+	if(key == 'q' || key == 'Q')
+	{
+		exit(0);
+	}
+}
+
+int main(int argc, char** argv)
+{
+	//glut initialization stuff:
+	// set the window size, display mode, and create the window
+	glutInit(&argc, argv);
+	glutInitWindowSize(600, 600);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+	glutCreateWindow("Spinning Cube");
+    
+	//enable Z buffer test, otherwise things appear in the order they're drawn
+	glEnable(GL_DEPTH_TEST);
+    
+	//setup the initial view
+	// change to projection matrix mode, set the extents of our viewing volume
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-2.5, 2.5, -2.5, 2.5, -2.5, 2.5);
+	
+    
+	//set clear colour to white
+	glClearColor(1, 1, 1, 0);
+    
+	glMatrixMode(GL_MODELVIEW);
+	//look down from a 45 deg. angle
+	glRotatef(45, 1, 0, 0);
+    
+    
+	//register glut callbacks for keyboard and display function
+	glutKeyboardFunc(kbd);
+	glutDisplayFunc(display);
+    
+	//start the program!
+	glutMainLoop();
+    
+	return 0;
+}
